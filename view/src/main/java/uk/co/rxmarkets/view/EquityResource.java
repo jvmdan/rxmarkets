7f42b01a9ecffd66a5139f8179870e0b0f1f4aca
Daniel Scarfe <daniel.scarfe@icloud.com>
2023-03-27 13:05:57 +0100

    View interface with random score generation. No plots.

--- view/src/main/java/uk/co/rxmarkets/view/EquityResource.java
+++ view/src/main/java/uk/co/rxmarkets/view/EquityResource.java
@@ -3,6 +3,9 @@ package uk.co.rxmarkets.view;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateExtension;
 import io.quarkus.qute.TemplateInstance;
+import org.jboss.resteasy.reactive.RestQuery;
+import uk.co.rxmarkets.model.scoring.Indicator;
+import uk.co.rxmarkets.model.scoring.Scoreboard;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,29 +18,36 @@ import java.util.List;
 @Path("/view")
 public class EquityResource {
 
+    /**
+     * The number of days worth of data we wish to plot.
+     */
+    private static final int N_DAYS = 28;
+
     @CheckedTemplate
     static class Templates {
 
-        static native TemplateInstance view(List<Item> items);
+        static native TemplateInstance view(String market, String ticker, List<Scoreboard> scores);
 
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item(new BigDecimal(10), "Apple"));
-        items.add(new Item(new BigDecimal(16), "Pear"));
-        items.add(new Item(new BigDecimal(30), "Orange"));
-        return EquityResource.Templates.view(items);
+    public TemplateInstance get(@RestQuery String market, @RestQuery String ticker) {
+        final List<Scoreboard> scores = new ArrayList<>();
+        for (int i = 0; i < N_DAYS; i++) {
+            scores.add(Scoreboard.random()); // TODO | Extract from API.
+        }
+        return EquityResource.Templates.view(market, ticker, scores);
     }
 
     /**
-     * This template extension method implements the "discountedPrice" computed property.
+     * This template extension method implements the "halveScore" computed property.
+     * This method exists to demonstrate that you can call functions in Java from
+     * within the HTML template. Simply call '.halveScore' on your given object.
      */
     @TemplateExtension
-    static BigDecimal discountedPrice(Item item) {
-        return item.price.multiply(new BigDecimal("0.9"));
+    static BigDecimal halveScore(Indicator i) {
+        return BigDecimal.valueOf(i.score()).multiply(new BigDecimal("0.5"));
     }
 
 }
